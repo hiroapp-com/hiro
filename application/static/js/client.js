f6bec95c50651d3808a4a746bc721e035d8647fe
Bruno Haid <bruno.haid@work.io>
2013-09-26 01:20:38 +0200

    Remove loading of GDrive etc API

--- application/static/js/client.js
+++ application/static/js/client.js
@@ -1743,11 +1743,11 @@ var WPCLib = {
 			}(document, 'script', 'facebook-jssdk'));	
 
 			// Init Google APIs
-			  (function() {
-			    var gd = document.createElement('script'); gd.type = 'text/javascript'; gd.async = true;
-			    gd.src = 'https://apis.google.com/js/client.js?onload=handleClientLoad';
-			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gd, s);
-			  })();		
+			//  (function() {
+			//    var gd = document.createElement('script'); gd.type = 'text/javascript'; gd.async = true;
+			//    gd.src = 'https://apis.google.com/js/client.js?onload=handleClientLoad';
+			//    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gd, s);
+			//  })();		
 
 			// Load filepicker.io
 			(function(a){if(window.filepicker){return}var b=a.createElement("script");b.type="text/javascript";b.async=!0;b.src=("https:"===a.location.protocol?"https:":"http:")+"//api.filepicker.io/v1/filepicker.js";var c=a.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c);var d={};d._queue=[];var e="pick,pickMultiple,pickAndStore,read,write,writeUrl,export,convert,store,storeUrl,remove,stat,setKey,constructWidget,makeDropPane".split(",");var f=function(a,b){return function(){b.push([a,arguments])}};for(var g=0;g<e.length;g++){d[e[g]]=f(e[g],d._queue)}window.filepicker=d})(document); 
