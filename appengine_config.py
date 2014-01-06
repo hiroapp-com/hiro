bef00ae3dea36067690dd59af71dc0100cc95218
Flo Lauber <fl-dev@qatfy.at>
2014-01-05 18:23:08 -0600

    minibug in profiler-conditional

--- appengine_config.py
+++ appengine_config.py
@@ -6,5 +6,5 @@ App Engine config
 def gae_mini_profiler_should_profile_production():
     import os
     from google.appengine.api import users
-    return os.environ['SHOW_PROFILER'] == 'yes' and users.is_current_user_admin()
+    return os.environ.get('SHOW_PROFILER') == 'yes' and users.is_current_user_admin()
 
