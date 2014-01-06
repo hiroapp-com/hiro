2ff402ec284f933485c7cd622f97c6bd016e6c23
Flo Lauber <fl-dev@qatfy.at>
2014-01-05 18:10:52 -0600

    add show-profiler conditionals

--- appengine_config.py
+++ appengine_config.py
@@ -3,9 +3,8 @@ App Engine config
 
 """
 
-
 def gae_mini_profiler_should_profile_production():
-    """Uncomment the first two lines to enable GAE Mini Profiler on production for admin accounts"""
-    # from google.appengine.api import users
-    # return users.is_current_user_admin()
-    return False
+    import os
+    from google.appengine.api import users
+    return os.environ['SHOW_PROFILER'] == 'yes' and users.is_current_user_admin()
+
