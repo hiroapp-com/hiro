08a7948cbfe2a58ee2938cb88d42937773ac6f54
Bruno Haid <bruno.haid@work.io>
2014-01-18 03:04:13 +0100

    Only track on production

--- application/static/js/client.js
+++ application/static/js/client.js
@@ -3055,7 +3055,7 @@ var Hiro = {
 				window.analytics.methods=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group","on","once","off"],window.analytics.factory=function(t){return function(){var a=Array.prototype.slice.call(arguments);return a.unshift(t),window.analytics.push(a),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var method=window.analytics.methods[i];window.analytics[method]=window.analytics.factory(method)}window.analytics.load=function(t){var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n)},window.analytics.SNIPPET_VERSION="2.0.8",
 				window.analytics.load("64nqb1cgw1");
 				// Identify user if user data is already loaded (eg Hiro loaded authed) 
-				if (Hiro.lib.user) analytics.identify(Hiro.lib.user.token,Hiro.lib.user);
+				if (Hiro.lib.user && Hiro.sys.production) analytics.identify(Hiro.lib.user.token,Hiro.lib.user);
 			},1000);
 
 			// Load facebook
@@ -3128,7 +3128,7 @@ var Hiro = {
 			if (!obj.name) Hiro.sys.user.getfirstname();	
 
 			// Identify user if loguser is called after everything is loaded (eg logging in after initial load) 
-			if (analytics.identify && typeof analytics.identify == 'function') analytics.identify(obj.token, obj);							
+			if (analytics.identify && typeof analytics.identify == 'function' && Hiro.sys.production) analytics.identify(obj.token, obj);							
 
 			// Set user object
 			this.user = obj;
