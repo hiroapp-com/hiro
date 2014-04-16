428eae61c98daafd72847631dc41a67ccb895d28
Bruno Haid <bruno.haid@work.io>
2014-04-16 22:04:37 +0200

    Fastbuttons and delayed-open-on-hover handlers

--- application/static/js/hiro.js
+++ application/static/js/hiro.js
@@ -19,12 +19,13 @@
 	Hiro.sync.store: Local storage abstractions	
 
 	Hiro.ui: Basic UI related functions like showing/hiding dialogs, sliding menu etc
+	Hiro.ui.fastbutton: Button event handlers that fire instantly on touch devices
+	Hiro.ui.touchy: Trigger events on hover/touchstart with an optional delay
 	Hiro.ui.swipe: Custom swipe functionality for touch devices
 	Hiro.ui.hprogres: Thin progress bar on very top of page
 
 	Hiro.sys: Core functionality like setup, logging etc
 	Hiro.sys.user: Internal user management, contactlist and methods
-	Hiro.sys.friends: Contact list management
 
 	Hiro.lib: External libraries like Facebook or analytics	
 
@@ -36,4 +37,197 @@
 var Hiro = {
 	version: '1.10.3',
 
+	// Sidebar and internal data structure
+	folio: {
+		// States
+		open: false,
+		// DOM IDs
+		id: 'folio',
+
+		// Init folio
+		init: function() {
+			var el = document.getElementById(this.id);
+
+			// Event setup
+			Hiro.ui.fastbutton.attach(el,Hiro.folio.folioclick);
+			Hiro.ui.touchy.attach(el,Hiro.folio.foliotouch,300);			
+		},
+
+		// If the user clicked somewhere in the folio
+		folioclick: function(event) {
+			console.log('Yes, the folio',event);
+		},
+
+		// If the user hovered over the folio with mouse/finger
+		foliotouch: function(event) {
+			console.log('Touched this!', event, this.open)
+		}
+	},
+
+	// Core system functionality
+	sys: {
+		inited: false,
+
+		// System setup, this is called once on startup and then calls inits for the various app parts 
+		init: function(tier) {
+			// Prevent initing twice
+			if (this.inited) return;
+
+			// Setup other app parts
+			Hiro.folio.init();
+		}
+	},
+
+	// All things ui. Click it, touch it, fix it, show it.
+	ui: {
+
+		// Handle clicks depending on device (mouse or touch)
+		fastbutton: {
+			// Current event details
+			x: undefined,
+			y: undefined,
+
+			// Attach initial event trigger
+			attach: function(element,handler) {
+				// Always attach click event
+				Hiro.util.registerEvent(element,'click', function(e) {Hiro.ui.fastbutton.fire(e,handler)});
+				// Optionally attach touchstart event for touch devices
+				if ('ontouchstart' in document.documentElement) Hiro.util.registerEvent(element,'touchstart', function(e) {Hiro.ui.fastbutton.fire(e,handler)});
+			},
+
+			// If the initial event is fired
+			fire: function(event,handler) {
+				if (event.type === 'click') {
+					// If the evnt is a click event we just execute the handler and quit
+					handler(event);					
+				} else if (event.type === 'touchstart') {
+					// In this case we attach a touchend event to wait for and set the start coordinates
+					console.log('touchend')
+				} else {
+					// Log error to see if any browsers fire unknown events
+					Hiro.sys.error('Fastbutton triggered unknown event: ', event);
+				}
+			},
+
+			// Reset the coordinates and remove touchend event
+			reset: function() {
+				Hiro.ui.fastbutton.x = Hiro.ui.fastbutton.y = undefined;
+			}			
+		},
+
+		// Attach events to areas that fire under certain conditions like hover and support delays
+		touchy: {
+			defaultdelay: 300,
+			timeout: null,
+			element: null,
+
+			// Attach initial trigger
+			attach: function(element,handler,delay) {
+				// Always attach mouse event
+				Hiro.util.registerEvent(element,'mouseover', function(e) {Hiro.ui.touchy.fire(e,element,handler,delay)});
+				// Attach touchstart event for touch devices
+				if ('ontouchstart' in document.documentElement)	Hiro.util.registerEvent(element,'touchstart', function(e) {Hiro.ui.touchy.fire(e,element,handler,delay)});
+			},
+
+			// If the event is fired
+			fire: function(event,element,handler,delay) {
+				if (event.type === 'touchstart') {
+					// If its a touch event, we turn this into a fastbutton without delay, but spatial limitation					
+					Hiro.ui.fastbutton.fire(event,handler);
+				} else if (event.type === 'mouseover') {
+					// If we already listen to this element but moved to a different subnode do nothing
+					if (element === this.element) return;
+					// Initiate the delayed event firing
+					delay = delay || this.defaultdelay;
+					this.element = element;
+					// Set timeout as local var (only one touchy at a time)
+					this.timeout = setTimeout(function() {
+						// If the timeout wasnt killed by the bounds handler, we execute the handler
+						handler(event);
+						// And clean up 
+						Hiro.ui.touchy.abort(element);
+					}, delay);
+					// Register mouseout event to clean things up once we leave target area
+					Hiro.util.registerEvent(element,'mouseout', Hiro.ui.touchy.boundschecker);				
+				} else {
+					// Log error to see if any browsers fire unknown events
+					Hiro.sys.error('Touchy triggered unknown event: ', event);
+				}
+			},
+
+			// Abort current touchy session if we leave target DOM area
+			boundschecker: function() {
+				// Get the DOM element the cursor is moving to
+				var target = event.relatedTarget || event.toElement;
+				// If we mouseout to the same or a contained DOM node do nothing				
+				if (target === this || this.contains(target)) return;
+				// If we leave the DOM are of interest, remove the handler and clean up
+				Hiro.util.releaseEvent(this,'mouseout',Hiro.ui.touchy.boundschecker);					
+				Hiro.ui.touchy.element = null;
+				if (Hiro.ui.touchy.timeout) Hiro.ui.touchy.abort();							
+			},
+
+			// Abort our timeout & clean up
+			abort: function() {
+				clearTimeout(this.timeout);				
+				this.timeout  = null;				
+			}
+
+		}
+
+	},
+
+	// Generic utilities like event attachment etc
+	util: {
+
+		// Cross browser event registration
+		registerEvent: function(obj, eventType, handler) {
+			if (obj.addEventListener) {
+				obj.addEventListener(eventType.toLowerCase(), handler, false);
+			}
+			else if (obj.attachEvent) {
+				obj.attachEvent('on'+eventType.toLowerCase(), handler);
+			}
+			else {
+				var et=eventType.toUpperCase();
+				if ((obj.Event) && (obj.Event[et]) && (obj.captureEvents)) obj.captureEvents(Event[et]);
+				obj['on'+eventType.toLowerCase()]=handler;
+			}
+		},
+
+		// Cross browser event removal
+		releaseEvent: function(obj, eventType, handler) {
+			if (obj.removeEventListener) {
+				obj.removeEventListener(eventType.toLowerCase(), handler, false);
+			}
+			else if (obj.detachEvent) {
+				try {
+	   				obj.detachEvent('on'+eventType.toLowerCase(), handler);
+				}
+				catch(e) {
+					Hiro.sys.log('',e);
+				}
+			}
+			else {
+				var et=eventType.toUpperCase();
+				if ((obj.Event) && (obj.Event[et]) && (obj.releaseEvents)) obj.releaseEvents(Event[et]);
+				et='on'+eventType.toLowerCase();
+				if ((obj[et]) && (obj[et]==handler)) obj.et=null;
+			}
+		},
+
+		// Cross browser default event prevention
+		stopEvent: function(e) {
+			if (!e) return;
+			if (e.preventDefault) {
+				e.preventDefault();
+			} else {
+				e.returnValue = false;
+			}
+			if (e.stopPropagation) {
+				e.stopPropagation();
+			} 
+			e.cancelBubble = true;
+		}		
+	}
 };
