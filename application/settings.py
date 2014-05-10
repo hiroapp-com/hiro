fee9eac59aaa4b0ffd5233a10ad6da7677b50f7b
Bruno Haid <bruno.haid@work.io>
2014-05-10 02:04:47 +0200

    Add go server dev url to settings

--- application/settings.py
+++ application/settings.py
@@ -25,6 +25,7 @@ DEBUG_MODE = False
 # Auto-set debug mode based on App Engine dev environ
 if 'SERVER_SOFTWARE' in os.environ and os.environ['SERVER_SOFTWARE'].startswith('Dev'):
     DEBUG_MODE = True
+    WS_URL = 'ws://localhost:8888/0/ws'
 
 DEBUG = DEBUG_MODE
 
