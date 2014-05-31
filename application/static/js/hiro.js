eca677f287503a7d7925b05588417cacab37924e
Bruno Haid <bruno.haid@work.io>
2014-06-01 01:14:30 +0200

    Newnote as far as possible, switch to uncompressed deep-diff for debugging

--- application/static/js/hiro.js
+++ application/static/js/hiro.js
@@ -79,7 +79,7 @@ var Hiro = {
 				switch (target.id) {
 					case 'archive':
 					case 'newnote':
-						Hiro.folio.newnote();
+						note = Hiro.folio.newnote();
 						break;
 					case 'settings':
 						console.log(target.id);
@@ -189,20 +189,35 @@ var Hiro = {
 			return d;			
 		},
 
-		// Move folio entry to top and resort rest of folio
+		// Move folio entry to top and resort rest of folio for both, local client and server versions
 		// TODO Bruno: Add sort by last own edit when we have it
 		sort: function(totop) {
-			var f = Hiro.data.get('folio','c');
+			var fc = Hiro.data.get('folio','c'),
+				fs = Hiro.data.get('folio','s');
 
 			// Move note by id to top of list
 			if (totop) {
-				for (i=0,l=f.length;i<l;i++) {
-					if (f[i].nid = totop) f.unshift(f.splice(i,1)[0]);
+				// Update client array
+				for (i=0,l=fc.length;i<l;i++) {
+					if (fc[i].nid == totop) {
+						// Remove item from array and insert at beginning
+						fc.unshift(fc.splice(i,1)[0]);
+						break;	
+					} 
 				}
+				// Update server array (we need the same order there)
+				for (i=0,l=fs.length;i<l;i++) {
+					if (fs[i].nid == totop) {
+						// Remove item from array and insert at beginning
+						fs.unshift(fs.splice(i,1)[0]);
+						break;	
+					} 
+				}				
 			}
 
 			// Save changes locally and repaint
-			Hiro.data.set('folio','c',f);
+			Hiro.data.quicksave('folio');
+			this.paint();
 		},
 
 		// Add a new note to folio and notes array, then open it 
@@ -219,26 +234,36 @@ var Hiro = {
 			// Convert id to string
 			id = id.toString();
 
-			// Build new note entry for folio
-			var folionote = {
+			// Build new note entries for folio, use status new on serverside to mark changes for deepdiff
+			var folioc = {
 				nid: id,
 				status: 'active'
 			}
+			var folios = {
+				nid: id,
+				status: 'new'
+			}
 
 			// Add new item to beginning of array
-			f.c.unshift(folionote);
+			f.c.unshift(folioc);
+			f.s.unshift(folios);		
 
 			// Build new note object for notes store
+			// TODO Bruno: Make sure we mark the notes as different once hync supports
 			var note = {
 				c: { text: '', title: '', tribe: [] },
+				s: { text: '', title: '', tribe: [] },				
 				sv: 0, cv: 0,
 				id: id,
 				kind: 'note'
 			}
 
-			// Add note,save and repaint folio
-			Hiro.data.quicksave('folio');			
-			Hiro.data.set('notes',id.toString(),note,'c','ADD',true);
+			// Add note and save						
+			Hiro.data.set('notes',id.toString(),note,'c','ADD',false);
+			Hiro.data.set('folio','',f);
+
+			// Return the id of the we just created
+			return id;
 		}			
 	},
 
@@ -353,7 +378,7 @@ var Hiro = {
 		set: function(store,key,value,source,type,paint) {
 			type = type || 'UPDATE',
 			source = source || 'c';
-			paint = paint || key.indexOf('.c.title');
+			paint = paint || (key && key.indexOf('.c.title') > -1);
 
 			// Create store if it doesn't exist yet
 			if (!this.stores[store]) this.stores[store] = {};
@@ -661,6 +686,8 @@ var Hiro = {
 		rx_res_sync_handler: function(data) {
 			var ack = this.queuelookup[data.tag];
 
+			console.log('Got: ',data);
+
 			// We do have a local msg, this is an ACK
 			if (ack) {
 				// Mark msg as complete
@@ -876,7 +903,8 @@ var Hiro = {
 				changes.clock = { sv : store['sv'] , cv : store['cv']++ };
 				changes.delta = {};				
 
-				// Create proper delta format
+				// Create proper delta format and apply changes to serverside
+				// Note: This is going to get very long and potentiall ugly...
 				for (i=0,l=d.length;i<l;i++) {
 					// If the last path element is a text element, we get a dmp delta from the rhs text & shadow
 					if (d[i].path == 'text') {
@@ -888,17 +916,26 @@ var Hiro = {
 
 						// Update shadow to latest value
 						store.c.shadow = store.c.text;
+
+					// If a new note was added to the folio	or had it's status changed
+					} else if (rootstoreid == 'folio' && d[i].item.lhs == 'new') {
+						// Add change to changes array
+						if (!changes.delta.add) changes.delta.add = [];
+						var c = { nid: store.c[d[i].index].nid, status: store.c[d[i].index].status};
+						// TODO Bruno: Add changes for submission once supported by hync
+						// changes.delta.add.push(c);
+
+						// Set 'new' value to client version value and disable b0rked applychange
+						// TODO Bruno: This currently affects all changes in this diff, think of better way
+						store.s[d[i].index].status = d[i].item.rhs;
+						uniform = false; 					
 					} else {
 						changes.delta[d[i].path] = [d[i].lhs,d[i].rhs];
 					}
-				}
 
-				// Apply changes to local serverstate object
-				if (uniform) {
-					for (i=0,l=d.length;i<l;i++) {
-						DeepDiff.applyChange(store.s,store.c,d[i]);
-					}
-				}	
+					// Apply changes to local serverstate object
+					if (uniform) DeepDiff.applyChange(store.s,store.c,d[i]);
+				}
 
 				// Mark store as tainted but do not persist yet for performance reasons
 				if (Hiro.data.unsaved.indexOf(rootstoreid) < 0) Hiro.data.unsaved.push(rootstoreid);			
@@ -1482,5 +1519,269 @@ var Hiro = {
 	}
 };
 
-// Minified Deep-Diff: https://github.com/flitbit/diff/
-!function(e){"use strict";var t,n,i=[];if(typeof global=="object"&&global){t=global}else if(typeof window!=="undefined"){t=window}else{t={}}n=t.DeepDiff;if(n){i.push(function(){if("undefined"!==typeof n&&t.DeepDiff===h){t.DeepDiff=n;n=e}})}function r(e,t){e.super_=t;e.prototype=Object.create(t.prototype,{constructor:{value:e,enumerable:false,writable:true,configurable:true}})}function f(e,t){Object.defineProperty(this,"kind",{value:e,enumerable:true});if(t&&t.length){Object.defineProperty(this,"path",{value:t,enumerable:true})}}function a(e,t,n){a.super_.call(this,"E",e);Object.defineProperty(this,"lhs",{value:t,enumerable:true});Object.defineProperty(this,"rhs",{value:n,enumerable:true})}r(a,f);function u(e,t){u.super_.call(this,"N",e);Object.defineProperty(this,"rhs",{value:t,enumerable:true})}r(u,f);function l(e,t){l.super_.call(this,"D",e);Object.defineProperty(this,"lhs",{value:t,enumerable:true})}r(l,f);function o(e,t,n){o.super_.call(this,"A",e);Object.defineProperty(this,"index",{value:t,enumerable:true});Object.defineProperty(this,"item",{value:n,enumerable:true})}r(o,f);function s(e,t,n){var i=e.slice((n||t)+1||e.length);e.length=t<0?e.length+t:t;e.push.apply(e,i);return e}function c(t,n,i,r,f,h,p){f=f||[];var b=f.slice(0);if(h){if(r&&r(b,h))return;b.push(h)}var d=typeof t;var v=typeof n;if(d==="undefined"){if(v!=="undefined"){i(new u(b,n))}}else if(v==="undefined"){i(new l(b,t))}else if(d!==v){i(new a(b,t,n))}else if(t instanceof Date&&n instanceof Date&&t-n!=0){i(new a(b,t,n))}else if(d==="object"&&t!=null&&n!=null){p=p||[];if(p.indexOf(t)<0){p.push(t);if(Array.isArray(t)){var y,m=t.length,g=function(e){i(new o(b,y,e))};for(y=0;y<t.length;y++){if(y>=n.length){i(new o(b,y,new l(e,t[y])))}else{c(t[y],n[y],g,r,[],null,p)}}while(y<n.length){i(new o(b,y,new u(e,n[y++])))}}else{var w=Object.keys(t);var D=Object.keys(n);w.forEach(function(f){var a=D.indexOf(f);if(a>=0){c(t[f],n[f],i,r,b,f,p);D=s(D,a)}else{c(t[f],e,i,r,b,f,p)}});D.forEach(function(t){c(e,n[t],i,r,b,t,p)})}p.length=p.length-1}}else if(t!==n){if(!(d==="number"&&isNaN(t)&&isNaN(n))){i(new a(b,t,n))}}}function h(t,n,i,r){r=r||[];c(t,n,function(e){if(e){r.push(e)}},i);return r.length?r:e}function p(e,t,n){if(n.path&&n.path.length){var i=e[t],r,f=n.path.length-1;for(r=0;r<f;r++){i=i[n.path[r]]}switch(n.kind){case"A":p(i[n.path[r]],n.index,n.item);break;case"D":delete i[n.path[r]];break;case"E":case"N":i[n.path[r]]=n.rhs;break}}else{switch(n.kind){case"A":p(e[t],n.index,n.item);break;case"D":e=s(e,t);break;case"E":case"N":e[t]=n.rhs;break}}return e}function b(e,t,n){if(!(n instanceof f)){throw new TypeError("[Object] change must be instanceof Diff")}if(e&&t&&n){var i=e,r,a;a=n.path.length-1;for(r=0;r<a;r++){if(typeof i[n.path[r]]==="undefined"){i[n.path[r]]={}}i=i[n.path[r]]}switch(n.kind){case"A":p(i[n.path[r]],n.index,n.item);break;case"D":delete i[n.path[r]];break;case"E":case"N":i[n.path[r]]=n.rhs;break}}}function d(e,t,n){if(e&&t){var i=function(i){if(!n||n(e,t,i)){b(e,t,i)}};c(e,t,i)}}Object.defineProperties(h,{diff:{value:h,enumerable:true},observableDiff:{value:c,enumerable:true},applyDiff:{value:d,enumerable:true},applyChange:{value:b,enumerable:true},isConflict:{get:function(){return"undefined"!==typeof n},enumerable:true},noConflict:{value:function(){if(i){i.forEach(function(e){e()});i=null}return h},enumerable:true}});if(typeof module!="undefined"&&module&&typeof exports=="object"&&exports&&module.exports===exports){module.exports=h}else{t.DeepDiff=h}}();
+// Raw Deep-Diff for debugging: https://github.com/flitbit/diff/
+;(function(undefined) {
+	"use strict";
+
+	var $scope
+	, conflict, conflictResolution = [];
+	if (typeof global == 'object' && global) {
+		$scope = global;
+	} else if (typeof window !== 'undefined'){
+		$scope = window;
+	} else {
+		$scope = {};
+	}
+	conflict = $scope.DeepDiff;
+	if (conflict) {
+		conflictResolution.push(
+			function() {
+				if ('undefined' !== typeof conflict && $scope.DeepDiff === accumulateDiff) {
+					$scope.DeepDiff = conflict;
+					conflict = undefined;
+				}
+			});
+	}
+
+	// nodejs compatible on server side and in the browser.
+  function inherits(ctor, superCtor) {
+    ctor.super_ = superCtor;
+    ctor.prototype = Object.create(superCtor.prototype, {
+      constructor: {
+        value: ctor,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+  }
+
+  function Diff(kind, path) {
+  	Object.defineProperty(this, 'kind', { value: kind, enumerable: true });
+  	if (path && path.length) {
+  		Object.defineProperty(this, 'path', { value: path, enumerable: true });
+  	}
+  }
+
+  function DiffEdit(path, origin, value) {
+  	DiffEdit.super_.call(this, 'E', path);
+  	Object.defineProperty(this, 'lhs', { value: origin, enumerable: true });
+  	Object.defineProperty(this, 'rhs', { value: value, enumerable: true });
+  }
+  inherits(DiffEdit, Diff);
+
+  function DiffNew(path, value) {
+  	DiffNew.super_.call(this, 'N', path);
+  	Object.defineProperty(this, 'rhs', { value: value, enumerable: true });
+  }
+  inherits(DiffNew, Diff);
+
+  function DiffDeleted(path, value) {
+  	DiffDeleted.super_.call(this, 'D', path);
+  	Object.defineProperty(this, 'lhs', { value: value, enumerable: true });
+  }
+  inherits(DiffDeleted, Diff);
+
+  function DiffArray(path, index, item) {
+  	DiffArray.super_.call(this, 'A', path);
+  	Object.defineProperty(this, 'index', { value: index, enumerable: true });
+  	Object.defineProperty(this, 'item', { value: item, enumerable: true });
+  }
+  inherits(DiffArray, Diff);
+
+  function arrayRemove(arr, from, to) {
+  	var rest = arr.slice((to || from) + 1 || arr.length);
+  	arr.length = from < 0 ? arr.length + from : from;
+  	arr.push.apply(arr, rest);
+  	return arr;
+  }
+
+  function deepDiff(lhs, rhs, changes, prefilter, path, key, stack) {
+  	path = path || [];
+  	var currentPath = path.slice(0);
+  	if (key) {
+  		if (prefilter && prefilter(currentPath, key)) return;
+  		currentPath.push(key);
+  	}
+  	var ltype = typeof lhs;
+  	var rtype = typeof rhs;
+  	if (ltype === 'undefined') {
+  		if (rtype !== 'undefined') {
+  			changes(new DiffNew(currentPath, rhs ));
+  		}
+  	} else if (rtype === 'undefined') {
+  		changes(new DiffDeleted(currentPath, lhs));
+  	} else if (ltype !== rtype) {
+  		changes(new DiffEdit(currentPath, lhs, rhs));
+  	} else if (lhs instanceof Date && rhs instanceof Date && ((lhs-rhs) != 0) ) {
+  		changes(new DiffEdit(currentPath, lhs, rhs));
+  	} else if (ltype === 'object' && lhs != null && rhs != null) {
+  		stack = stack || [];
+  		if (stack.indexOf(lhs) < 0) {
+  			stack.push(lhs);
+  			if (Array.isArray(lhs)) {
+  				var i
+  				, len = lhs.length
+  				, ea = function(d) {
+  					changes(new DiffArray(currentPath, i, d));
+  				};
+  				for(i = 0; i < lhs.length; i++) {
+  					if (i >= rhs.length) {
+  						changes(new DiffArray(currentPath, i, new DiffDeleted(undefined, lhs[i])));
+  					} else {
+  						deepDiff(lhs[i], rhs[i], ea, prefilter, [], null, stack);
+  					}
+  				}
+  				while(i < rhs.length) {
+  					changes(new DiffArray(currentPath, i, new DiffNew(undefined, rhs[i++])));
+  				}
+  			} else {
+  				var akeys = Object.keys(lhs);
+  				var pkeys = Object.keys(rhs);
+  				akeys.forEach(function(k) {
+  					var i = pkeys.indexOf(k);
+  					if (i >= 0) {
+  						deepDiff(lhs[k], rhs[k], changes, prefilter, currentPath, k, stack);
+  						pkeys = arrayRemove(pkeys, i);
+  					} else {
+  						deepDiff(lhs[k], undefined, changes, prefilter, currentPath, k, stack);
+  					}
+  				});
+  				pkeys.forEach(function(k) {
+  					deepDiff(undefined, rhs[k], changes, prefilter, currentPath, k, stack);
+  				});
+  			}
+  			stack.length = stack.length - 1;
+  		}
+  	} else if (lhs !== rhs) {
+      if(!(ltype === "number" && isNaN(lhs) && isNaN(rhs))) {
+  		  changes(new DiffEdit(currentPath, lhs, rhs));
+      }
+  	}
+  }
+
+  function accumulateDiff(lhs, rhs, prefilter, accum) {
+  	accum = accum || [];
+  	deepDiff(lhs, rhs,
+  		function(diff) {
+  			if (diff) {
+  				accum.push(diff);
+  			}
+  		},
+  		prefilter);
+  	return (accum.length) ? accum : undefined;
+  }
+
+	function applyArrayChange(arr, index, change) {
+		if (change.path && change.path.length) {
+			// the structure of the object at the index has changed...
+			var it = arr[index], i, u = change.path.length - 1;
+			for(i = 0; i < u; i++){
+				it = it[change.path[i]];
+			}
+			switch(change.kind) {
+				case 'A':
+					// Array was modified...
+					// it will be an array...
+					applyArrayChange(it[change.path[i]], change.index, change.item);
+					break;
+				case 'D':
+					// Item was deleted...
+					delete it[change.path[i]];
+					break;
+				case 'E':
+				case 'N':
+					// Item was edited or is new...
+					it[change.path[i]] = change.rhs;
+					break;
+			}
+		} else {
+			// the array item is different...
+			switch(change.kind) {
+				case 'A':
+					// Array was modified...
+					// it will be an array...
+					applyArrayChange(arr[index], change.index, change.item);
+					break;
+				case 'D':
+					// Item was deleted...
+					arr = arrayRemove(arr, index);
+					break;
+				case 'E':
+				case 'N':
+					// Item was edited or is new...
+					arr[index] = change.rhs;
+					break;
+			}
+		}
+		return arr;
+	}
+
+	function applyChange(target, source, change) {
+		if (!(change instanceof Diff)) {
+			throw new TypeError('[Object] change must be instanceof Diff');
+		}
+		if (target && source && change) {
+			var it = target, i, u;
+			u = change.path.length - 1;
+			for(i = 0; i < u; i++){
+				if (typeof it[change.path[i]] === 'undefined') {
+					it[change.path[i]] = {};
+				}
+				it = it[change.path[i]];
+			}
+			switch(change.kind) {
+				case 'A':
+					// Array was modified...
+					// it will be an array...
+					applyArrayChange(it[change.path[i]], change.index, change.item);
+					break;
+				case 'D':
+					// Item was deleted...
+					delete it[change.path[i]];
+					break;
+				case 'E':
+				case 'N':
+					// Item was edited or is new...
+					it[change.path[i]] = change.rhs;
+					break;
+				}
+			}
+		}
+
+	function applyDiff(target, source, filter) {
+		if (target && source) {
+			var onChange = function(change) {
+				if (!filter || filter(target, source, change)) {
+					applyChange(target, source, change);
+				}
+			};
+			deepDiff(target, source, onChange);
+		}
+	}
+
+	Object.defineProperties(accumulateDiff, {
+
+		diff: { value: accumulateDiff, enumerable:true },
+		observableDiff: { value: deepDiff, enumerable:true },
+		applyDiff: { value: applyDiff, enumerable:true },
+		applyChange: { value: applyChange, enumerable:true },
+		isConflict: { get: function() { return 'undefined' !== typeof conflict; }, enumerable: true },
+		noConflict: {
+			value: function () {
+				if (conflictResolution) {
+					conflictResolution.forEach(function (it) { it(); });
+					conflictResolution = null;
+				}
+				return accumulateDiff;
+			},
+			enumerable: true
+		}
+	});
+
+	if (typeof module != 'undefined' && module && typeof exports == 'object' && exports && module.exports === exports) {
+		module.exports = accumulateDiff; // nodejs
+	} else {
+		$scope.DeepDiff = accumulateDiff; // other... browser?
+	}
+}());
