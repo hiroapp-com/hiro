caeffea1f1c517dfc27944292879c6936973bbe2
Flo Lauber <fl-dev@qatfy.at>
2014-08-07 02:18:40 -0700

    add reconnect and retry modifications to jsonclient

--- jsonclient.py
+++ jsonclient.py
@@ -10,13 +10,32 @@ import json, socket, itertools
 class JSONClient(object):
 
     def __init__(self, addr):
-        self.socket = socket.create_connection(addr)
+        self.addr = addr
         self.id_counter = itertools.count()
+        try:
+            self.socket = socket.create_connection(addr)
+        except:
+            self.socket = None
 
     def __del__(self):
+        if not self.socket:
+            return
         self.socket.close()
 
-    def call(self, name, *params):
+    def connect(self):
+        if self.socket:
+            return True
+        try:
+            self.socket = socket.create_connection(self.addr)
+            return True
+        except socket.error as msg: 
+            print "connection to comm listener at {} failed with msg {}".format(self.addr, msg)
+            self.socket = None
+            return False
+
+    def call(self, name, retry=1, *params):
+        if not self.connect():
+            return None
         request = dict(id=next(self.id_counter),
                     params=list(params),
                     method=name)
@@ -24,6 +43,13 @@ class JSONClient(object):
 
         # This must loop if resp is bigger than 4K
         response = self.socket.recv(4096)
+        if len(response) == 0:
+            # send failed even after connect. something most be wrong
+            # save send-request somewhere for later resend
+            self.socket = None
+            if retry > 0:
+                return self.call(name, (retry-1), *params)
+            return None
         response = json.loads(response.decode())
 
         if response.get('id') != request.get('id'):
