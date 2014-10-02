58c768414d30d2e425d96901158ee80fe9ad50cf
Bruno Haid <bruno.haid@work.io>
2014-10-02 05:00:43 +0200

    Miniimprovement to trigger new deploy

--- views.py
+++ views.py
@@ -30,7 +30,7 @@ facebook = oauth.remote_app('facebook',
 facebook.tokengetter(lambda: session.get('oauth_token'))
 
 # Version number fetched from Git, this is either the tag (if latest commit was tagged) or "tag-commitsAheadOfTag-currentCommitHash"
-gittag = subprocess.check_output(["git", "describe"]).rstrip('\n')
+gittag = subprocess.check_output(["git", "describe"]).splitlines()[0]
 gitname = subprocess.check_output(["git", "tag", "-l", "-n1"]).splitlines()[-1][16:]
 
 
