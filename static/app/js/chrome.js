8622740a2f4b8b21e1e33915748b25280e9ae528
Bruno <bruno.haid@gmail.com>
2014-12-06 09:30:41 +0800

    Update chrome app

--- static/app/js/chrome.js
+++ static/app/js/chrome.js
@@ -1,5 +1,8 @@
 // If the user clicks the browser icon
-chrome.browserAction.onClicked.addListener(spawn)
+chrome.browserAction.onClicked.addListener(spawn);
+
+// Or the apps icon
+chrome.app.runtime.onLaunched.addListener(spawn);
 
 // Create a new tab
 function spawn() { chrome.tabs.create({ url: 'https://www.hiroapp.com'	}) }
